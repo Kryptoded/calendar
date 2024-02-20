7666d29c865f59f911eaea965942bc3ca0864e48
andrewAndrew <kao99@bk.ru>
2024-02-20 21:59:09 +0300

    make a main buisness logic of calendar

--- js/calendar.js
+++ js/calendar.js
@@ -14,32 +14,100 @@ const $calendar = {
                 </div>
             </div>`,
     months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-    currentDate: new Date(),
     render(htmlElement) {
+        this.currentDate = new Date()
+        this.currentDate.setHours(0,0,0,0)
         htmlElement.insertAdjacentHTML('afterbegin', this.html)
         this.createMonths()
+        this.addEvents()
+    },
+    addEvents(){
+        this.notDisableDays = Array.from(document.body.querySelectorAll(".day:not(.disable)"))
+        this.notDisableDays.forEach(day => {
+            day.addEventListener('click',this.clickOnDay.bind(this))
+            day.addEventListener('mouseover', this.hoverOnDay.bind(this))
+        })
+    },
+    removeStartEnd() {
+        this.dateStart = null
+        this.dateEnd = null
+        this.notDisableDays.forEach((item) =>{
+            item.classList.remove('range-start')
+            item.classList.remove('range-end')
+            item.classList.remove('hovered')
+        })
+    },
+    clickOnDay(event) {
+        if (!this.dateStart) {
+            this.dateStart = new Date(event.target.dataset['date'])
+            event.target.classList.add('range-start')
+            return;
+        }
+        if(this.dateStart && !this.dateEnd) {
+            if (new Date(event.target.dataset['date']) > this.dateStart) {
+                this.dateEnd = new Date(event.target.dataset['date'])
+                event.target.classList.add('range-end')
+            }
+            return;
+        }
+        if (this.dateStart && this.dateEnd) {
+            this.removeStartEnd()
+            this.dateStart = new Date(event.target.dataset['date'])
+            event.target.classList.add('range-start')
+            return;
+        }
+    },
+    hoverOnDay(event) {
+        const currentDate = new Date(event.target.dataset['date'])
+        if (this.dateStart && !this.dateEnd) {
+            this.notDisableDays.forEach((item)=>{
+                const itemDate = new Date(item.dataset['date'])
+                if ( itemDate <= currentDate && itemDate > this.dateStart ) {
+                    item.classList.add('hovered')
+                } else {
+                    item.classList.remove('hovered')
+                }
+            })
+        } else {
+            return
+        }
     },
     getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
         let day = date.getDay();
         if (day == 0) day = 7; // сделать воскресенье (0) последним днем
         return day - 1;
-      },
+    },
+    isSameDate(d1, d2) {
+        return d1.getTime() === d2.getTime()
+    },
+    createDayHTML(d) {
+        if (d < this.currentDate) {
+            return `<div class='day disable'> ${d.getDate()} </div>`
+        } 
+        if (this.isSameDate(d, this.currentDate)) {
+            return `<div class='day today' data-date='${d}'> ${d.getDate()} </div>`
+        }
+        else {
+            return `<div class='day' data-date='${d}'> ${d.getDate()} </div>`
+        }
+    },
+
     getMonthDays(month, year) {
         let days = `<div class='week'>`
         d = new Date(year, month)
-        for (let i = 0; i < this.getDay(d); i++) {
-            days += `<div class='day'> </div>`
+        for (let i = 0; i < this.getDay(d); i++) { //Дополнить пустыми днями до начала недели
+            days += `<div class='day disable'> </div>`
         }
-        while (d.getMonth() === month) {
-            days += `<div class='day'> ${d.getDate()} </div>`
+        while (d.getMonth() === month) { // основной цикл по заполнению месяца
+            days += this.createDayHTML(d)
             if (this.getDay(d) === 6) {
                 days += `</div> <div class='week'>`
             }
             d.setDate(d.getDate() +1)
         }
-        if (this.getDay(d) !== 0) {
+        if (this.getDay(d) !== 0) { // Закончить пустыми днями до конца недели
             for (let i = this.getDay(d); i < 7; i ++) {
-                days += `<div class='day'></div>`
+                days += `<div class='day disable'></div>`
             }
         }
         
